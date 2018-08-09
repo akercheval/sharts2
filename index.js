6c9b584606af32fad96b7c66fddf9c80d2fe06f0
Adam Ross Kercheval <akerchev@amazon.com>
2018-08-09 17:02:30 -0400

    login page, event for sign-on (and disconnection?) and client counting

--- index.js
+++ index.js
@@ -3,22 +3,42 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-var users = 0;
+var users = [];
 
 app.get('/', function(req, res){
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/home', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  users += 1;
-  io.emit('chat message', "somebody else has joined!");
-  io.emit('chat message', "There are now " + io.engine.clientsCount + " people online.");
+  if (io.engine.clientsCount == 1) {
+    io.emit('chat message', "You're the first one here, lucky you!");
+  } else {
+    io.emit('chat message', "Hello! " + users.slice(0, -1).join(" and ") + " are already here!");
+  }
+  
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
+  // This fires at weird times. Try uncommenting it and look at the console when you use the page.
+  // Also, every time it fires it reduces io.engine.clientsCount by one (I think?) which screws up
+  // the ability to tell how many people are online, which we need
+  /*
   socket.on('disconnect', function() {
-    io.sockets.emit('chat message', "somebody logged off :(");
-    io.sockets.emit('chat message', "There are now " + io.engine.clientsCount + "people online.");
+    console.log("disconnection");
+    io.sockets.emit('chat message', "somebody logged off!");
+    io.sockets.emit('chat message', "There are now " + io.engine.clientsCount + " people online.");
+  });
+  */
+  
+  socket.on('name', function(name) {
+    io.emit('chat message', name + " just logged on!");
+    users.push(name);
+    io.emit('chat message', "There are now " + io.engine.clientsCount + " people online.");
   });
 });
 
